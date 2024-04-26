f4607bb3f9673c29a5ae20292082da2eb41f7d69
fersayago <sayago.fernando@gmail.com>
2024-04-26 16:52:46 -0300

    feat: :passport_control: authentication with clerk and landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,12 +1,55 @@
-
 import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-green-500 font-bold">
-        Hello Owl!
-      </p>
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" alt="hero" fill />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutra-600 max-w-[480px] text-center">
+          Learn and master new languages with OwlyOracle.
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" signInFallbackRedirectUrl="/learn">
+                <Button size="lg" variant="secondary" className="w-full mb-2">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                fallbackRedirectUrl="/learn"
+                signUpFallbackRedirectUrl="/learn"
+              >
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
