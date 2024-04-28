64be2ba438b2b3034284aa6605e9d14c30269c99
fersayago <sayago.fernando@gmail.com>
2024-04-28 05:29:12 -0300

    feat: :sparkles: learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import UserProgress from "@/components/user-progress";
+
+import Header from "./header";
 
 const Learn = () => {
   return (
-    <div>Learn</div>
-  )
-}
+    <div className="flex gap-[48px] px-6">
+      <FeedWrapper>
+        <Header title="Spanish"/>
+      </FeedWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg"}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+    </div>
+  );
+};
 
-export default Learn
+export default Learn;
