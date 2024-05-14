6f83b98087bfb990cd5f23af16e286f09f1ad058
fersayago <sayago.fernando@gmail.com>
2024-05-14 11:42:07 -0300

    build: :arrow_up: update drizzle orm

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: 'pg',
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
-  }
-} satisfies Config;
+    url: process.env.DATABASE_URL!,
+  },
+});
