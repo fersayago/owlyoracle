9b13a425293f0120cd36a7a113dc3e6e7a2f3f46
fersayago <sayago.fernando@gmail.com>
2024-06-14 17:14:35 -0300

    feat: :sparkles: course progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,19 +1,36 @@
-import { getUnits, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+
 import FeedWrapper from "@/components/feed-wrapper";
-import StickyWrapper from "@/components/sticky-wrapper";
 import UserProgress from "@/components/user-progress";
+import StickyWrapper from "@/components/sticky-wrapper";
+import {
+  getCourseProgress,
+  getUnits,
+  getUserProgress,
+  getLessonPercentage,
+} from "@/db/queries";
 
 import Header from "./header";
-import { redirect } from "next/navigation";
 import Unit from "./unit";
 
 const Learn = async () => {
   const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const getLessonPercentageData = getLessonPercentage();
   const unitsData = getUnits();
 
-  const [userProgress, units] = await Promise.all([userProgressData, unitsData]);
+  const [userProgress, units, courseProgress, lessonPercentage] = await Promise.all([
+    userProgressData,
+    unitsData,
+    courseProgressData,
+    getLessonPercentageData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  if (!courseProgress) {
     redirect("/courses");
   }
 
@@ -29,8 +46,8 @@ const Learn = async () => {
               description={unit.description}
               title={unit.title}
               lessons={unit.lessons}
-              activeLesson={undefined}
-              activeLessonPercentage={0}
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
             />
           </div>
         ))}
