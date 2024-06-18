c39af5a8e97d78692d579ea0f5229a686f769ce8
fersayago <sayago.fernando@gmail.com>
2024-06-18 01:08:25 -0300

    feat: :sparkles: challenge footer

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { challengeOptions, challenges } from "@/db/schema";
 
 import Header from "./header";
+import Footer from "./footer";
 import Challenge from "./challenge";
 import QuestionBubble from "./question-bubble";
 
@@ -36,8 +37,16 @@ const Quiz = ({
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number | undefined>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+      setSelectedOption(id);
+  }
 
   const title =
     challenge.type === "ASSIST"
@@ -62,9 +71,9 @@ const Quiz = ({
               )}
               <Challenge
                 options={options}
-                onSelect={() => {}}
-                status="none"
-                selectedOption={undefined}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -72,6 +81,11 @@ const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer
+        disabled={!selectedOption}
+        status={status}
+        onCheck={() => {}}
+      />
     </>
   );
 };
